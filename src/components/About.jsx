711dab495658afdb6d4a4c809522a41a5dc66843
Madiha Umair <madiha.altaf@yahoo.com>
2023-05-25 13:40:04 -0400

    background change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
     <div>
       <div className="about">
         <div id="about-text-portion">
-          <h1>Hi, I'm Madiha <span>...</span></h1>
+          <h1>Hi, I'm <span id="name">Madiha</span> <span id="exclamation">!</span></h1>
           <h1> - Full Stack Developer -</h1>
             <p id="about-text">I am passionate about coding and love to build things that can help people.</p>
             <p id="about-text-cont">I am always looking for new challenges and opportunities to learn and grow as a developer.</p>
